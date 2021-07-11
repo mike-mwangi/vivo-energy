5b55239dc2651ede430e93266d6863880362814e
mohamed-gudle <58801991+mohamed-gudle@users.noreply.github.com>
2021-07-11 10:20:19 +0300

    map update on contact page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,9 +18,11 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('about', function () {
+Route::get('/about', function () {
     return view('about');
 });
+Route::resource('contact', ContactController::class);
+
 
 
 
