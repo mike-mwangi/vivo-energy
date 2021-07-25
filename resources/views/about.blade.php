b42acade36c3bc075891ab579910e45e9f11aaae
HopeDiane <48009649+HopeDiane@users.noreply.github.com>
2021-07-25 23:41:54 +0300

    Update about.blade.php

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -324,16 +324,6 @@
                             </div>
                         </div>
                     </div>
-                    <!-- Copy-Right -->
-                    <div class="row align-items-center">
-                        <div class="col-xl-12 ">
-                            <div class="footer-copy-right">
-                                <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </div>
       </div>
